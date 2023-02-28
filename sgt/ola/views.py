849cae99cfa1e835e095ea79225518498986d34d
maryane-castro <maryane.castro993@gmail.com>
2023-02-28 16:24:49 -0300

    .

--- sgt/ola/views.py
+++ sgt/ola/views.py
@@ -7,5 +7,5 @@ def index(resquest):
     return HttpResponse('Olá, Mundo!')
 
 def saudacoes(resquets, name):
-    return HttpResponse("Nome: " + name)
+    return HttpResponse(f"<h1 style='color:rgb(150, 80, 219)'>Olá {name}</h1>")
 # Create your views here.
