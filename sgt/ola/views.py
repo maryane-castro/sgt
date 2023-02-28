dd1f63a2852cccb5d91e43af483b3a4524faa0ad
maryane-castro <maryane.castro993@gmail.com>
2023-02-28 16:09:50 -0300

    hg

--- sgt/ola/views.py
+++ sgt/ola/views.py
@@ -6,5 +6,6 @@ from django.http import HttpResponse
 def index(resquest):
     return HttpResponse('Olá, Mundo!')
 
-
+def saudacoes(resquets, name):
+    return HttpResponse("Nome: " + name)
 # Create your views here.
